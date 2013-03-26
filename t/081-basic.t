rurban/Compress-Bzip2

commit 94e6dedbcf8ec954ede3fea05ecab56d87ed9339

[cpan #48128] support memBunzip BZh header w/o extra size prefix
grow dest buf dynamically. Tests in t/040-memory.t

Bzip2.xs
@@ -264,6 +264,7 @@ long bzfile_clear_totals( obj ) bzFile *obj; {
 #endif
   obj->total_in = 0;
   obj->total_out = 0;
+  return 0;
 }
 
 #ifdef CAN_PROTOTYPE
@@ -477,7 +478,7 @@ int bzfile_setparams( obj, param, setting ) bzFile* obj; char* param; int settin
     savsetting = -1;
   }
 
-  if (obj->verbosity>1)
+  if (obj->verbosity>1) {
     if ( savsetting == -1 )
       PerlIO_printf(PerlIO_stderr(), "debug: bzfile_setparams invalid param %s => %d\n", param, setting);
     else
@@ -485,7 +486,7 @@ int bzfile_setparams( obj, param, setting ) bzFile* obj; char* param; int settin
 	PerlIO_printf(PerlIO_stderr(), "debug: bzfile_setparams query %s is %d\n", param, savsetting);
       else
 	PerlIO_printf(PerlIO_stderr(), "debug: bzfile_setparams set %s (is %d) => %d\n", param, savsetting, setting);
-
+  }
   return savsetting;
 }
 
@@ -1286,17 +1287,18 @@ int bzfile_read( obj, bufferOfUncompress, nUncompress ) bzFile* obj; char *buffe
 
     if ( obj->strm.avail_in == 0 ) {
       /* still zero bytes in the input hopper?  why? ... */
-      if ( !obj->pending_io_error )
+      if ( !obj->pending_io_error ) {
 	if ( obj->run_progress != 0 && obj->run_progress != 10 ) {
 	  if ( !bytes_uncompressed_count ) {
 	    BZ_SETERR(obj, BZ_UNEXPECTED_EOF, NULL);
 
-	    if (obj->verbosity>=2)
+	    if (obj->verbosity>=2) {
 	      PerlIO_printf(PerlIO_stderr(), "debug: bzfile_read got an unexpected EOF, run_progress=%d, avail_in=%d, avail_out=%d\n",
 			    obj->run_progress,
 			    obj->strm.avail_in,
 			    obj->strm.avail_out
 			    );
+	    }
 
 	    return -1;
 	  }
@@ -1305,41 +1307,44 @@ int bzfile_read( obj, bufferOfUncompress, nUncompress ) bzFile* obj; char *buffe
 	  obj->pending_io_error = True;
 	  obj->io_error = BZ_UNEXPECTED_EOF;
 
-	  if (obj->verbosity>=2)
+	  if (obj->verbosity>=2) {
 	    PerlIO_printf(PerlIO_stderr(), "debug: bzfile_read got an unexpected EOF, run_progress=%d, set pending with %d bytes to go, avail_in=%d, avail_out=%d\n",
 			  obj->run_progress,
 			  bytes_uncompressed_count,
 			  obj->strm.avail_in,
 			  obj->strm.avail_out
 			  );
+	  }
 	}
 	else {
 	  obj->pending_io_error = True;
 	  obj->io_error = BZ_IO_EOF;
 
-	  if (obj->verbosity>=2)
+	  if (obj->verbosity>=2) {
 	    PerlIO_printf(PerlIO_stderr(), "debug: bzfile_read got an EOF, run_progress=%d, set pending with %d bytes to go, avail_in=%d, avail_out=%d\n",
 			  obj->run_progress,
 			  bytes_uncompressed_count,
 			  obj->strm.avail_in,
 			  obj->strm.avail_out
 			  );
+	  }
 	}
-
+      }
       /* if no io_error is pending, this is a proper end of file */
       return bytes_uncompressed_count;
     }
     else {
-      if ( obj->run_progress == 1 || obj->run_progress == 11 )
+      if ( obj->run_progress == 1 || obj->run_progress == 11 ) {
 	/* indicate we have data to uncompress */
 	obj->run_progress = obj->run_progress == 1 ? 2 : 12;
-      else if ( obj->run_progress == 0 || obj->run_progress == 10 ) {
+      } else if ( obj->run_progress == 0 || obj->run_progress == 10 ) {
 	ret = BZ2_bzDecompressInit ( &(obj->strm), obj->verbosity, obj->small );
 
 	if (ret != BZ_OK) {
-	  if (obj->verbosity>1)
+	  if (obj->verbosity>1) {
 	    warn("Error: bzfile_read: BZ2_bzDecompressInit error %d on %d, %d\n",
 		 ret, obj->verbosity, obj->small);
+	  }
 
 	  BZ_SETERR(obj, ret, NULL);
 	  return -1;
@@ -1356,7 +1361,7 @@ int bzfile_read( obj, bufferOfUncompress, nUncompress ) bzFile* obj; char *buffe
       else
 	ret = BZ2_bzDecompress( &(obj->strm) );
 
-      if (obj->verbosity>=4)
+      if (obj->verbosity>=4) {
 	PerlIO_printf(PerlIO_stderr(), "\ndebug: bzfile_read BZ2_bzDecompress ret %d, run_progress=%d, avail_in=%d/%d, avail_out=%d/%d\n",
 		      ret,
 		      obj->run_progress,
@@ -1365,6 +1370,7 @@ int bzfile_read( obj, bufferOfUncompress, nUncompress ) bzFile* obj; char *buffe
 		      tracker,
 		      obj->strm.avail_out
 		      );
+      }
 
       if (ret != BZ_OK && ret != BZ_STREAM_END) {
 	if ( ret != BZ_DATA_ERROR_MAGIC || !obj->allowUncompressedRead ) {
@@ -1821,17 +1827,36 @@ memBunzip(sv)
 
     in = (unsigned char*)SvPV(sv, len);
     if (len < 5 + 3 || in[0] < 0xf0 || in[0] > 0xf1) {
-      warn("invalid buffer (too short %d or bad marker %d)",len,in[0]);
-      XSRETURN_UNDEF;
+      if (len > 16 && in[0] == 'B' && in[1] == 'Z' && in[2] == 'h') {
+	in_len = len;
+	out_len = 0;
+	RETVAL = newSV(len * 10);
+      } else {
+	warn("invalid buffer (too short %d or bad marker %d)",len,in[0]);
+	XSRETURN_UNDEF;
+      }
+    } else {
+      in_len = len - 5;
+      out_len = (in[1] << 24) | (in[2] << 16) | (in[3] << 8) | in[4];
+      RETVAL = newSV(out_len > 0 ? out_len : 1);
     }
-    in_len = len - 5;
-    out_len = (in[1] << 24) | (in[2] << 16) | (in[3] << 8) | in[4];
-    RETVAL = newSV(out_len > 0 ? out_len : 1);
     SvPOK_only(RETVAL);
     out = (unsigned char*)SvPVX(RETVAL);
-    new_len = out_len;
-    err = BZ2_bzBuffToBuffDecompress((char*)out,&new_len,(char*)in+5,in_len,0,0);
-    if (err != BZ_OK || new_len != out_len) {
+    new_len = out_len ? out_len : len * 3; /* guess out size */
+    err = BZ2_bzBuffToBuffDecompress((char*)out,&new_len,
+      out_len ? (char*)in+5:(char *)in, in_len,0,0);
+    while (!out_len && (err == BZ_OUTBUFF_FULL)) {
+      new_len = SvLEN(RETVAL) * 2;
+      SvGROW(RETVAL, new_len);
+      err = BZ2_bzBuffToBuffDecompress((char*)out,&new_len,
+				       (char *)in,in_len,0,0);
+    }
+    if (err != BZ_OK) {
+      SvREFCNT_dec(RETVAL);
+      BZ_SETERR(NULL, err, ix==1 ? "decompress" : "memBunzip");
+      XSRETURN_UNDEF;
+    }
+    if (out_len && new_len != out_len) {
       SvREFCNT_dec(RETVAL);
       BZ_SETERR(NULL, err, ix==1 ? "decompress" : "memBunzip");
       XSRETURN_UNDEF;

t/081-basic.t
@@ -13,7 +13,7 @@ use Compress::Bzip2 qw(compress decompress);
 
 # globals
 
-my $Level = 1;
+my $Level = 6;
 my ($In,$Out) = (0,0);
 my @AlNum = ('A'..'Z','a'..'z','0'..'9',' ');
 
@@ -76,7 +76,7 @@ for my $random(1..100) {
   $str .= ($AlNum[rand @AlNum] x rand 1000) for 1..100+rand 900;
   ok(try($str),"long string $random");
 }
-#diag(sprintf "compression ratio %.2f%%",100*$Out/$In);
+diag(sprintf "compression ratio %.2f%%",100*$Out/$In);
 
 # binary strings
 
@@ -87,5 +87,5 @@ for my $random(1..100) {
   $str .= chr(rand 256) for 1..1000+rand 9000;
   ok(try($str),"binary string $random");
 }
-#diag(sprintf "compression ratio %.2f%%",100*$Out/$In);
+diag(sprintf "compression ratio %.2f%%",100*$Out/$In);
 

lib/Compress/Bzip2.pm
@@ -913,6 +913,11 @@ I<an> error in numeric context. Use B<bzerror()> to check for specific
 I<bzlib> errors. The I<bzcat> example below shows how the variable can
 be used safely.
 
+=head2 B<$bz-E<gt>prefix>
+
+Returns the additional 5 byte header which is prepended to the bzip2
+header  starting with C<BZh> when using memBzip/compress.
+
 =head1 Compress::Bzip2 1.03 COMPATIBILITY
 
 While the 2.x thread forked off of 1.00, another line of development
@@ -1100,17 +1105,17 @@ provided which provide similar functionality.
 
 =head2 B<$compressed = memBzip($buffer);>
 
-Compresses B<$source>. If successful it returns the compressed
+Compresses B<$buffer>. If successful it returns the compressed
 data. Otherwise it returns I<undef>.
 
 The buffer parameter can either be a scalar or a scalar reference.
 
 Essentially, an in-memory bzip file is created. It creates a minimal
-bzip header.
+bzip header, which adds 5 bytes before the bzip2 specific BZh header.
 
 =head2 B<$uncompressed = memBunzip($buffer);>
 
-Uncompresses B<$source>. If successful it returns the uncompressed
+Uncompresses B<$buffer>. If successful it returns the uncompressed
 data. Otherwise it returns I<undef>.
 
 The source buffer can either be a scalar or a scalar reference.

t/040-memory.t
@@ -1,7 +1,6 @@
 # -*- mode: perl -*-
 
-use Test::More tests => 10;
-#use Test::More qw(no_plan);
+use Test::More tests => 15;
 
 BEGIN {
   use_ok('Compress::Bzip2', qw(:utilities :bzip1));
@@ -17,6 +16,8 @@ and the Momewrathes outgrabe
 
 my $compress = memBzip( $string );
 my $uncompress = memBunzip( $compress );
+
+ok( substr($compress,5,16) =~ /^BZh/, "compressed starts with bzip magic header" );
 
 ok( $compress ne $string, "string was not inouted" );
 ok( length($compress)-10 < length($string), "string compression - ".length($compress).' vs '.length($string) );
@@ -36,3 +37,23 @@ $uncompress = decompress( $compress );
 ok( $compress ne $string, "bzip1 string was not inouted" );
 ok( length($compress)-10 < length($string), "bzip1 string compression - ".length($compress).' vs '.length($string) );
 ok( $uncompress eq $string, "bzip1 decompress is same as the original" );
+
+do 't/lib.pl';
+
+# allow plain BZh files with memBunzip also
+my $INFILE = catfile( qw(bzlib-src sample0.bz2) );
+local $/ = undef;
+open( IN, "< $INFILE" ) or die "$INFILE: $!";
+binmode IN;
+my $sample0 = <IN>;
+close( IN );
+
+$uncompress = memBunzip( $sample0 );
+ok( $uncompress, "sample0 uncompressed w/o header" );
+like( $uncompress, qr/^That\'s great, it starts with an earthquake/ );
+
+my $header = pack("c", 0xf0);
+$header .= pack "N", $uncompress ? length($uncompress) : 2027;
+$uncompress = memBunzip( $header . $sample0 );
+ok( $uncompress, "sample0 uncompressed w/ header" );
+like( $uncompress, qr/^That\'s great, it starts with an earthquake/ );

commit 77505e314c4904bed20e7f0d686f53c79f575530

fixed decompression of empty strings, broken since 94e6dedbcf
94e6dedbcf support memBunzip BZh header w/o extra size prefix
had the special case with out_len = 0. But out_len = 0 is also used for empty
strings. Use a new noprefix var instead.

Bzip2.xs
@@ -1819,9 +1819,10 @@ memBunzip(sv)
     unsigned char *	in;
     unsigned char *	out;
     unsigned int	in_len;
-    unsigned int	out_len;
+    unsigned int        out_len;
     unsigned int	new_len;
-    int		err;
+    int			err;
+    int 		noprefix = 0;
 
   CODE:
   {
@@ -1834,7 +1835,8 @@ memBunzip(sv)
     if (len < 5 + 3 || in[0] < 0xf0 || in[0] > 0xf1) {
       if (len > 16 && in[0] == 'B' && in[1] == 'Z' && in[2] == 'h') {
 	in_len = len;
-	out_len = 0;
+	out_len = len * 5; /* guess uncompressed size */
+	noprefix = 1;
 	RETVAL = newSV(len * 10);
       } else {
 	warn("invalid buffer (too short %ld or bad marker %d)",len,in[0]);
@@ -1847,10 +1849,10 @@ memBunzip(sv)
     }
     SvPOK_only(RETVAL);
     out = (unsigned char*)SvPVX(RETVAL);
-    new_len = out_len ? out_len : len * 5; /* guess uncompressed size */
+    new_len = out_len;
     err = BZ2_bzBuffToBuffDecompress((char*)out,&new_len,
-      out_len ? (char*)in+5:(char *)in, in_len,0,0);
-    while (!out_len && (err == BZ_OUTBUFF_FULL)) {
+      noprefix ? (char*)in:(char *)in+5, in_len,0,0);
+    while (noprefix && (err == BZ_OUTBUFF_FULL)) {
       new_len = SvLEN(RETVAL) * 2;
       SvGROW(RETVAL, new_len);
       err = BZ2_bzBuffToBuffDecompress((char*)out,&new_len,
@@ -1861,7 +1863,7 @@ memBunzip(sv)
       BZ_SETERR(NULL, err, ix==1 ? "decompress" : "memBunzip");
       XSRETURN_UNDEF;
     }
-    if (out_len && new_len != out_len) {
+    if (!noprefix && new_len != out_len) {
       SvREFCNT_dec(RETVAL);
       BZ_SETERR(NULL, err, ix==1 ? "decompress" : "memBunzip");
       XSRETURN_UNDEF;

t/081-basic.t
@@ -22,7 +22,8 @@ my @AlNum = ('A'..'Z','a'..'z','0'..'9',' ');
 
 sub try {
   my $str = shift;
-  my $comp = compress($str,$Level);
+  my $level = shift || $Level;
+  my $comp = compress($str,$level);
   return 0 if not defined $comp;
   my $orig = decompress($comp);
   return 0 if not defined $orig;
@@ -37,7 +38,7 @@ sub try {
 # some short strings
 
 my $sum;
-ok(try(''),'empty string');
+ok(try('', 1),'empty string');
 ok((eval { try(undef) }, $@),'undef fails');
 $sum = 0;
 $sum += try($_) for @AlNum;
@@ -60,10 +61,12 @@ is($sum,5,'reference tests');
 
 $sum = 0;
 for my $random(1..100) {
-  $Level = 9 if 80 == $random;
+  my $level = $Level;
+  $level = 1 if 20 == $random;
+  $level = 9 if 80 == $random;
   my $str = '';
   $str .= $AlNum[rand @AlNum] for 1..rand 100;
-  $sum += try($str);
+  $sum += try($str, $level);
 }
 is($sum,100,'100 random strings');
 
@@ -71,10 +74,12 @@ is($sum,100,'100 random strings');
 
 ($In,$Out) = (0,0);
 for my $random(1..100) {
-  $Level = 1 if 20 == $random;
+  my $level = $Level;
+  $level = 1 if 20 == $random;
+  $level = 9 if 80 == $random;
   my $str = '';
   $str .= ($AlNum[rand @AlNum] x rand 1000) for 1..100+rand 900;
-  ok(try($str),"long string $random");
+  ok(try($str, $level),"long string $random");
 }
 diag(sprintf "compression ratio %.2f%%",100*$Out/$In);
 
@@ -82,10 +87,12 @@ diag(sprintf "compression ratio %.2f%%",100*$Out/$In);
 
 ($In,$Out) = (0,0);
 for my $random(1..100) {
-  $Level = 9 if 80 == $random;
+  my $level = $Level;
+  $level = 1 if 20 == $random;
+  $level = 9 if 80 == $random;
   my $str = '';
   $str .= chr(rand 256) for 1..1000+rand 9000;
-  ok(try($str),"binary string $random");
+  ok(try($str, $level),"binary string $random");
 }
 diag(sprintf "compression ratio %.2f%%",100*$Out/$In);
 