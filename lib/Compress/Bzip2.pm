c5d67c4be1a348db9691092c2d56e4e9c97096c8
Reini Urban <rurban@cpanel.net>
2015-01-15 10:46:00 +0100

    Oops, re-release as 2.20

--- lib/Compress/Bzip2.pm
+++ lib/Compress/Bzip2.pm
@@ -2,7 +2,7 @@
 # Author   : Rob Janes
 # Created  : 14 April 2005
 # Modified : 2015-01-15 rurban
-# Version  : 2.19
+# Version  : 2.20
 #
 #     Copyright (c) 2005 Rob Janes. All rights reserved.
 #     This program is free software; you can redistribute it and/or
@@ -12,7 +12,7 @@
 package Compress::Bzip2;
 
 use 5.006;
-our $VERSION = "2.19";
+our $VERSION = "2.20";
 use strict;
 use warnings;
 
