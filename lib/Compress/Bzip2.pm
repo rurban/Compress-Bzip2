1e745b4141d06cb9edfe80b01586e2014b0feca2
Reini Urban <rurban@cpanel.net>
2015-12-08 15:46:41 +0100

    Release 2.24
    - Silence gcc-5 optimizer warning (RT #105647)
      with internal BUILD_BZLIB=1 (upstream, but not a real problem)
    - Improve kwalitee test

--- lib/Compress/Bzip2.pm
+++ lib/Compress/Bzip2.pm
@@ -1,8 +1,8 @@
 # File	   : Bzip2.pm
 # Author   : Rob Janes
 # Created  : 14 April 2005
-# Modified : 2015-06-08 rurban
-# Version  : 2.23
+# Modified : 2015-12-08 rurban
+# Version  : 2.24
 #
 #     Copyright (c) 2005 Rob Janes. All rights reserved.
 #     This program is free software; you can redistribute it and/or
@@ -12,7 +12,7 @@
 package Compress::Bzip2;
 
 use 5.006;
-our $VERSION = "2.23";
+our $VERSION = "2.24";
 use strict;
 use warnings;
 
